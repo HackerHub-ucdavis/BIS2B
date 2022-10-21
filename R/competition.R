fe838f3d070a73dae1927cedbf28dee72e308979
Ethan He <yfhe@ucdavis.edu>
2022-10-20 21:52:55 -0700

    feat: add plot for lab4

--- R/competition.R
+++ R/competition.R
@@ -1,4 +1,5 @@
 # lab 4
+library(ggplot2)
 
 #' clean the lab data sheet
 #'
@@ -11,30 +12,44 @@
 clean_lab4_excel <- function(raw) {
     clean <- raw[, !(names(raw) %in% c("Grp. Name", "Mean", "SD", "SE"))]
     clean <- clean[rowSums(is.na(clean)) == 0, ]
+    class(clean) <- c("lab4", class(clean))
     clean
 }
 
-#' Test for a Treatment
-#'
-#' t test for a treatment by given compoents
-#'
 #' @param data a cleaned dataframe (from clean_lab4_excel)
-#' @param treatement string the name of this treatement
-#' @param components list of string components names to compare
-#' @param  alpha float Significance Level
-#' @return a complete table of test results
+#' @return mean, standard deviation, standard error
+#' @examples
+#' summary(clean_lab4_excel(raw_df))
 #' @export
-test_treatment <- function(data, treatement, components, alpha = 0.05) {
+summary.lab4 <- function(data) {
     # compute the intermediate data
     memo <- data.frame(
         Mean = rowMeans(data),
         SD = apply(data, 1, sd)
     )
     memo$SE <- memo$SD / sqrt(ncol(data))
-    memo <- round(memo, 2)
+    list(
+        data = round(memo, 2),
+        df = 2 * ncol(data) - 2
+    )
+}
 
+#' Test for a Treatment
+#'
+#' t test for a treatment by given compoents
+#'
+#' @param memo a cleaned dataframe (from summerize_lab4)
+#' @param treatement string the name of this treatement
+#' @param components list of string components names to compare
+#' @param  alpha float Significance Level
+#' @return a complete table of test results
+#' @examples
+#' summ <- summary(clean_lab4_excel(raw_df))
+#' test_treatment(summ$data, treatments[i], components, summ$df)
+#' @export
+test_treatment <- function(memo, treatement, components, df, alpha = 0.05) {
     # critical value
-    crit <- qt(1 - alpha, 2 * ncol(data) - 2)
+    crit <- qt(1 - alpha, df)
 
     # t test
     l <- length(components)
@@ -64,4 +79,27 @@ test_treatment <- function(data, treatement, components, alpha = 0.05) {
         result[i, ]$Reject <- t > crit
     }
     result
+}
+
+#' @param component_df dataframe of one component
+#' @param title string name of the plot
+#' @return ggplot graph object
+#' @examples
+#' Mean	    SD	    SE	    treatment	morph
+#' 22.44	8.04	2.32	No	        0
+#' 14.55	2.68	0.77	No	        1
+#' 22.06	12.29	3.55	Intramorph	0
+#' 17.03	4.53	1.31	Intramorph	1
+#' 16.72	3.97	1.15	Intermorph	0
+#' 17.67	7.27	2.10	Intermorph	1
+#' @import ggplot2
+#' @export
+plot_mean_err <- function(component_df, title = "") {
+    ggplot(component_df, aes(x = treatment, y = Mean, color = morph)) +
+        geom_point() +
+        geom_errorbar(aes(ymin = Mean - SE, ymax = Mean + SE),
+            width = .2,
+            position = position_dodge(0.05)
+        ) +
+        ggtitle(title)
 }
